589cd82bc574b7d7c20eab85fc656e5d0ec62544
autsada <autsada.t@gmail.com>
2023-05-06 10:28:25 +0700

    add upload page

--- app/(protect-routes)/upload/page.tsx
+++ app/(protect-routes)/upload/page.tsx
@@ -1,5 +1,32 @@
 import React from "react"
+import { MdPlayArrow } from "react-icons/md"
+import { AiFillRead } from "react-icons/ai"
 
 export default function Upload() {
-  return <div>Upload</div>
+  return (
+    <>
+      <h5>Upload content</h5>
+
+      <div className="mt-5 flex flex-col sm:flex-row gap-y-5 sm:gap-y-0 sm:gap-x-5">
+        <div className="w-full sm:w-[50%] md:w-[40%] lg:w-[35%] text-center px-5 py-10 border border-gray-200 rounded-md">
+          <p>Upload videos, shorts, or podcasts.</p>
+          <div className="my-10">
+            <div className="w-[120px] h-[120px] mx-auto rounded-full flex items-center justify-center bg-orange-500">
+              <MdPlayArrow size={50} color="white" />
+            </div>
+          </div>
+          <button className="btn-dark w-[100px]">Upload</button>
+        </div>
+        <div className="w-full sm:w-[50%] md:w-[40%] lg:w-[35%] text-center px-5 py-10 border border-gray-200 rounded-md">
+          <p>Create blogs.</p>
+          <div className="my-10">
+            <div className="w-[120px] h-[120px] mx-auto rounded-full flex items-center justify-center bg-blue-500">
+              <AiFillRead size={50} color="white" />
+            </div>
+          </div>
+          <button className="btn-light w-[100px]">Create</button>
+        </div>
+      </div>
+    </>
+  )
 }
