55f52f30e8efea8928d214bfbf501e3eb45e9bfb
autsada <autsada.t@gmail.com>
2023-05-07 09:35:56 +0700

    add app loader ui

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,8 +2,8 @@ import React from "react"
 
 export default function NotFound() {
   return (
-    <>
+    <div className="w-full h-full py-10 flex items-center justify-center">
       <h6>Page not found</h6>
-    </>
+    </div>
   )
 }
