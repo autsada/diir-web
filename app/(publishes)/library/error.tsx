02e3848e0bead0bde3d071fa5ff9f1777bb01128
autsada <autsada.t@gmail.com>
2023-06-01 09:16:22 +0700

    update error page styles

--- app/(publishes)/library/error.tsx
+++ app/(publishes)/library/error.tsx
@@ -1,5 +1,7 @@
 "use client" // Error components must be Client Components
 
+import { useRouter } from "next/navigation"
+
 export default function Error({
   error,
   reset,
@@ -7,19 +9,22 @@ export default function Error({
   error: Error
   reset: () => void
 }) {
+  const router = useRouter()
+
   return (
     <div className="text-center py-10">
-      <h4 className="error font-semibold text-4xl">
-        Sorry, something went wrong!
-      </h4>
+      <h4 className="error font-semibold text-4xl">Something not right!</h4>
+      <p className="mt-4 mb-8">
+        Please try refreshing the page or click below to go to homepage.
+      </p>
       <button
-        className="btn-dark mt-10 px-8 rounded-full"
+        className="btn-dark px-8 rounded-full"
         onClick={
           // Attempt to recover by trying to re-render the segment
-          () => reset()
+          () => router.replace("/")
         }
       >
-        Try again
+        Home
       </button>
     </div>
   )
