ebc65add2407ffe1a8c462850cfed580612e4da9
autsada <autsada.t@gmail.com>
2023-06-29 12:37:21 +0700

    fixed walletconnect wss error

--- ethereum/config.tsx
+++ ethereum/config.tsx
@@ -1,5 +1,5 @@
 import { EthereumClient, w3mConnectors, w3mProvider } from "@web3modal/ethereum"
-import { configureChains, createConfig, WagmiConfig } from "wagmi"
+import { configureChains, createConfig } from "wagmi"
 import { mainnet, goerli, localhost } from "@wagmi/chains"
 
 import { WALLET_CONNECT_PROJECT_ID } from "@/lib/constants"
@@ -10,7 +10,7 @@ const projectId = WALLET_CONNECT_PROJECT_ID
 const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
 export const wagmiConfig = createConfig({
   autoConnect: true,
-  connectors: w3mConnectors({ projectId, version: 1, chains }),
+  connectors: w3mConnectors({ projectId, chains }),
   publicClient,
 })
 export const ethereumClient = new EthereumClient(wagmiConfig, chains)
